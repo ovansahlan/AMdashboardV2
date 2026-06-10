d9b44fb47a92129b5f9a37c0040c795cfa6e1b17
ovansahlan <ovansahlan@gmail.com>
2026-06-11 00:17:51 +0700

    test

--- api/getDashboard.js
+++ api/getDashboard.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
       targetRange = 'Historis_Bulanan!A:AZ';
     } else if (requestedTab === 'getRawDaily') {
       // Anda bisa ubah ini jadi 'Raw_DailyJuni26!A:Z' jika namanya memang itu
-      targetRange = 'Raw_daily!A:Z'; 
+      targetRange = 'Raw_DailyJuni26!A:Z'; 
     }
 
     try {
